4c5f32c3bdf706e064669386a066190476bee1d3
inespuigserver <inespuigserver@gmail.com>
2026-09-22 17:42:34 +0200

    ordenar_array

--- modulo.py
+++ modulo.py
@@ -15,3 +15,11 @@ def modulo(a, b):
     division_entera = a // b
     return a - (division_entera * b)
 print(modulo(10, 3))
+
+def ordenar_array(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
